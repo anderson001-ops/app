b17a4ed6e59c62a467aa58cb552a871658d81e99
anderson001-ops <joelcapera2023@gmail.com>
2025-11-17 22:41:59 -0500

    login user

--- backend/src/main/java/com/app/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/app/backend/dto/LoginRequest.java
@@ -13,6 +13,9 @@ public class LoginRequest {
     public String getPassword(){
         return password;
     }
+    public void setUsername(String username){
+        this.username = username;
+    }
     public void setPassword(String password){
         this.password=password;
     }
